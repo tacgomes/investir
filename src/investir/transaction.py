90979d45dd0df463f567c8606735f0a1e8136886
Tiago Gomes <tiago.avv+oss@gmail.com>
2024-04-17 17:48:01 +0100

    Transaction: Add methods to merge and split orders
    Add methods to merge and split orders. They will be used when
    calculating capital gains/losses in a following commit.

--- src/investir/transaction.py
+++ src/investir/transaction.py
@@ -39,6 +39,62 @@ class Order(Transaction, ABC):
     def price(self) -> Decimal:
         return self.amount / self.quantity
 
+    def split(self, split_quantity: Decimal):  # -> tuple[Self, Self] (3.11+)
+        assert self.quantity >= split_quantity
+
+        match_amount = self.price * split_quantity
+        match_quantity = split_quantity
+        match_fees = self.fees / self.quantity * split_quantity
+
+        remainder_amount = self.amount - match_amount
+        remainder_quantity = self.quantity - match_quantity
+        remainder_fees = self.fees - match_fees
+
+        match = type(self)(
+            self.timestamp,
+            amount=match_amount,
+            ticker=self.ticker,
+            quantity=match_quantity,
+            fees=match_fees,
+            note=f'Splitted from order {self.id}')
+
+        remainder = type(self)(
+            self.timestamp,
+            amount=remainder_amount,
+            ticker=self.ticker,
+            quantity=remainder_quantity,
+            fees=remainder_fees,
+            note=f'Splitted from order {self.id}')
+
+        return match, remainder
+
+    @staticmethod
+    def merge(*orders: 'Order') -> 'Order':
+        assert len(orders) > 1
+
+        ticker = orders[0].ticker
+        assert all(order.ticker == ticker for order in orders)
+
+        order_class = type(orders[0])
+
+        timestamp = orders[0].timestamp.replace(
+            hour=0, minute=0, second=0, microsecond=0)
+
+        amount = Decimal(sum(order.amount for order in orders))
+        quantity = Decimal(sum(order.quantity for order in orders))
+        fees = Decimal(sum(order.fees for order in orders))
+
+        note = 'Merged from orders '
+        note += ','.join(str(order.id) for order in orders)
+
+        return order_class(
+            timestamp,
+            amount=amount,
+            ticker=ticker,
+            quantity=quantity,
+            fees=fees,
+            note=note)
+
 
 class Acquisition(Order):
     @property
