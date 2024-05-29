c70512868fb0a251b2309feb6a332655dc4d29e4
Tiago Gomes <tiago.avv+oss@gmail.com>
2024-05-29 20:13:12 +0100

    Freetrade: Improve parsing logging
    Log each row in a more dense way by not showing keys that have an empty
    string as value.

--- src/investir/parser/utils.py
+++ src/investir/parser/utils.py
@@ -11,3 +11,7 @@ MIN_TIMESTAMP: Final = datetime(2008, 4, 6, tzinfo=timezone.utc)
 
 def read_decimal(val: str, default: Decimal = Decimal("0.0")) -> Decimal:
     return Decimal(val) if val.strip() else default
+
+
+def dict2str(d: dict[str, str]) -> str:
+    return str({k: v for k, v in d.items() if v.strip()})
