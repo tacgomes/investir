93c59a2aeca4e7dbaff2b19bdf74d90faf612abf
Tiago Gomes <tiago.avv+oss@gmail.com>
2024-05-29 18:49:32 +0100

    Logging: Update colour scheme

--- src/investir/logging.py
+++ src/investir/logging.py
@@ -3,19 +3,20 @@ from typing import Final
 
 
 class CustomFormatter(logging.Formatter):
-    LIGHT_CYAN: Final = "\033[0;96m"
-    GREEN: Final = "\033[0;32m"
-    YELLOW: Final = "\033[38;5;226m"
-    RED: Final = "\033[38;5;196m"
-    BOLD_RED: Final = "\033[31;1m"
-    RESET: Final = "\033[0m"
+    CSI: Final = "\033["
+    BRIGHT_CYAN: Final = f"{CSI}96m"
+    BOLD_WHITE: Final = f"{CSI}1;37m"
+    BRIGHT_YELLOW: Final = f"{CSI}93m"
+    RED: Final = f"{CSI}31m"
+    BOLD_RED: Final = f"{CSI}1;31m"
+    RESET: Final = f"{CSI}0m"
 
     def __init__(self, fmt: str) -> None:
         super().__init__()
         self.formats = {
-            logging.DEBUG: self.LIGHT_CYAN + fmt + self.RESET,
-            logging.INFO: self.GREEN + fmt + self.RESET,
-            logging.WARNING: self.YELLOW + fmt + self.RESET,
+            logging.DEBUG: self.BRIGHT_CYAN + fmt + self.RESET,
+            logging.INFO: self.BOLD_WHITE + fmt + self.RESET,
+            logging.WARNING: self.BRIGHT_YELLOW + fmt + self.RESET,
             logging.ERROR: self.RED + fmt + self.RESET,
             logging.CRITICAL: self.BOLD_RED + fmt + self.RESET,
         }
