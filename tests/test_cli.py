17f2e1fc221b59b3ca4badf0edf8e7718929dcb7
Tiago Gomes <tiago.avv+oss@gmail.com>
2024-09-24 22:29:18 +0100

    Tests: Handle os.EX_OK not being defined for Microsoft Windows

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -12,6 +12,7 @@ from investir.cli import app
 PROJECT_DIR = Path(__file__).parent.parent
 DATA_FILE = str(PROJECT_DIR / "data" / "freetrade.csv")
 OUTPUTS_DIR = PROJECT_DIR / "tests" / "test_cli"
+EX_OK = getattr(os, "EX_OK", 0)
 
 
 @pytest.fixture(name="runner")
@@ -105,7 +106,7 @@ def test_cli_output(request, runner, cmd, expected):
         expected_output.write_text(result.stdout)
     else:
         assert expected_output.exists()
-        assert result.exit_code == os.EX_OK
+        assert result.exit_code == EX_OK
         assert result.stdout == expected_output.read_text()
 
 
@@ -113,69 +114,69 @@ def test_capital_gains_multiple_input_files(runner):
     data_file1 = str(PROJECT_DIR / "data" / "trading212_2021-2022.csv")
     data_file2 = str(PROJECT_DIR / "data" / "trading212_2022-2023.csv")
     result = runner.invoke(app, ["--offline", "capital-gains", data_file1, data_file2])
-    assert result.exit_code == os.EX_OK
+    assert result.exit_code == EX_OK
     expected_output = OUTPUTS_DIR / "capital_gains"
     assert result.stdout == expected_output.read_text()
 
 
 def test_orders_command_no_results(runner):
     result = runner.invoke(app, ["orders", "--tax-year", "2008", DATA_FILE])
-    assert result.exit_code == os.EX_OK
+    assert result.exit_code == EX_OK
     assert not result.stdout
     assert not result.stderr
 
 
 def test_dividends_command_no_results(runner):
     result = runner.invoke(app, ["dividends", "--tax-year", "2008", DATA_FILE])
-    assert result.exit_code == os.EX_OK
+    assert result.exit_code == EX_OK
     assert not result.stdout
     assert not result.stderr
 
 
 def test_transfers_command_no_results(runner):
     result = runner.invoke(app, ["transfers", "--tax-year", "2008", DATA_FILE])
-    assert result.exit_code == os.EX_OK
+    assert result.exit_code == EX_OK
     assert not result.stdout
     assert not result.stderr
 
 
 def test_interest_command_no_results(runner):
     result = runner.invoke(app, ["interest", "--tax-year", "2008", DATA_FILE])
-    assert result.exit_code == os.EX_OK
+    assert result.exit_code == EX_OK
     assert not result.stdout
     assert not result.stderr
 
 
 def test_capital_gains_command_no_results(runner):
     result = runner.invoke(app, ["capital-gains", "--tax-year", "2008", DATA_FILE])
-    assert result.exit_code == os.EX_OK
+    assert result.exit_code == EX_OK
     assert not result.stdout
     assert not result.stderr
 
 
 def test_holdings_no_results(runner):
     result = runner.invoke(app, ["holdings", "--ticker", "NOTFOUND", DATA_FILE])
-    assert result.exit_code == os.EX_OK
+    assert result.exit_code == EX_OK
     assert not result.stdout
     assert not result.stderr
 
 
 def test_orders_command_mutually_exclusive_filters(runner):
     result = runner.invoke(app, ["orders", "--acquisitions", "--disposals", DATA_FILE])
-    assert result.exit_code != os.EX_OK
+    assert result.exit_code != EX_OK
     assert not result.stdout
     assert "Usage:" in result.stderr
 
 
 def test_transfers_command_mutually_exclusive_filters(runner):
     result = runner.invoke(app, ["transfers", "--deposits", "--withdrawals", DATA_FILE])
-    assert result.exit_code != os.EX_OK
+    assert result.exit_code != EX_OK
     assert not result.stdout
     assert "Usage:" in result.stderr
 
 
 def test_capital_gains_command_mutually_exclusive_filters(runner):
     result = runner.invoke(app, ["capital-gains", "--gains", "--losses", DATA_FILE])
-    assert result.exit_code != os.EX_OK
+    assert result.exit_code != EX_OK
     assert not result.stdout
     assert "Usage:" in result.stderr
