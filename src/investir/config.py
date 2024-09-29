d419c36844e2913c23ceba9b63b8f45d223db017
Tiago Gomes <tiago.avv+oss@gmail.com>
2024-09-29 15:04:14 +0100

    Config: Implement minor improvements

--- src/investir/config.py
+++ src/investir/config.py
@@ -1,5 +1,5 @@
+import logging
 from dataclasses import dataclass
-from logging import INFO
 from pathlib import Path
 
 from platformdirs import user_cache_dir
@@ -7,12 +7,12 @@ from platformdirs import user_cache_dir
 
 @dataclass
 class Config:
-    log_level: int = INFO
-    use_colour: bool = True
     strict: bool = True
     offline: bool = False
     cache_file: Path = Path(user_cache_dir()) / "investir" / "securities.yaml"
     include_fx_fees: bool = True
+    log_level: int = logging.INFO
+    use_colour: bool = True
 
 
 config = Config()
