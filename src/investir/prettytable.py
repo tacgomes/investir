f5da710d79299a1b4baddbb30c6f772a1de5b3d6
Tiago Gomes <tiago.avv+oss@gmail.com>
2024-12-01 14:44:07 +0000

    PrettyTable: Improve the appearance of tables
    Improve the appearance of tables by implementing the following changes:
    * Avoid rendering most of the frame for a table.
    * Make the text on field titles to appear stylised in bold.
    * Remove the minimal width for the "Name" field.
    * Show dates in a format more common in the United Kingdom.

--- src/investir/prettytable.py
+++ src/investir/prettytable.py
@@ -1,34 +1,49 @@
 from collections.abc import Callable
+from datetime import date
 from decimal import Decimal
 from typing import Any
 
 import prettytable
 
+from investir.utils import boldify, unboldify
+
+
+def date_format(format: str) -> Callable[[str, Any], str]:
+    def _date_format(_field, val) -> str:
+        if isinstance(val, date):
+            return val.strftime(format)
+        return val
+
+    return _date_format
+
 
 def decimal_format(precision: int) -> Callable[[str, Any], str]:
     def _decimal_format(_field, val) -> str:
-        if val is None:
-            return ""
-        elif isinstance(val, Decimal):
+        if isinstance(val, Decimal):
             return f"{val:.{precision}f}"
+        elif isinstance(val, str):
+            return val
         else:
-            return f"{val}"
+            return ""
 
     return _decimal_format
 
 
 class PrettyTable(prettytable.PrettyTable):
     def __init__(self, *args, **kwargs) -> None:
+        kwargs["field_names"] = list(map(lambda f: boldify(f), kwargs["field_names"]))
+
         super().__init__(*args, **kwargs)
 
-        for f in kwargs["field_names"]:
-            match f.split()[0], f.split()[-1]:
-                case ("Name", _):
-                    self.custom_format["Name"] = lambda _, val: f"{val:8}"
-                    self.align[f] = "l"
+        for f in self.field_names:
+            plain_f = unboldify(f)
 
+            match plain_f.split()[0].strip(), plain_f.split()[-1]:
+                case ("Date", _):
+                    self.custom_format[f] = date_format("%d/%m/%Y")
+                    self.align[f] = "l"
                 case ("Quantity", _):
-                    self.custom_format["Quantity"] = decimal_format(8)
+                    self.custom_format[f] = decimal_format(8)
                     self.align[f] = "r"
 
                 case (_, "(£)") | (_, "(%)"):
@@ -38,11 +53,21 @@ class PrettyTable(prettytable.PrettyTable):
                 case _:
                     self.align[f] = "l"
 
+        self.hrules = prettytable.HEADER
         self.vrules = prettytable.NONE
+        self.invisible_fields: set[str] = set()
+
+    def hide_field(self, field_name: str) -> None:
+        self.invisible_fields.add(field_name)
 
     def to_string(self, leading_nl: bool = True) -> str:
         nl = "\n" if leading_nl else ""
-        return f"{nl}{self.get_string()}\n"
+
+        fields = [
+            f for f in self.field_names if unboldify(f) not in self.invisible_fields
+        ]
+
+        return f"{nl}{self.get_string(fields=fields)}\n"
 
     def __bool__(self) -> bool:
         return len(self.rows) > 0
