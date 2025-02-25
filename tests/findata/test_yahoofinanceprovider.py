e43050dc34fed0f42a92e3d61172b1843202befb
Tiago Gomes <tiago.avv+oss@gmail.com>
2025-02-25 18:47:16 +0000

    Tests: Split test fixture in two independent ones

--- tests/findata/test_yahoofinanceprovider.py
+++ tests/findata/test_yahoofinanceprovider.py
@@ -34,17 +34,22 @@ AMZN_SPLITS = [
 ]
 
 
-@pytest.fixture(name="ticker_mocker")
-def _ticker_mocker(mocker) -> Callable:
-    def _method(
-        info: Mapping[str, Any] | Exception, splits: pd.Series | None = None
-    ) -> None:
-        mocker.patch(
+@pytest.fixture(name="ticker_info")
+def _ticker_info(mocker) -> Callable:
+    def _method(info: Mapping[str, Any] | Exception) -> None:
+        return mocker.patch(
             "yfinance.Ticker.info",
             return_value=info,
             new_callable=mocker.PropertyMock,
         )
-        mocker.patch(
+
+    return _method
+
+
+@pytest.fixture(name="ticker_splits")
+def _ticker_splits(mocker) -> Callable:
+    def _method(splits: pd.Series | None = None) -> None:
+        return mocker.patch(
             "yfinance.Ticker.splits",
             return_value=splits,
             new_callable=mocker.PropertyMock,
@@ -53,11 +58,11 @@ def _ticker_mocker(mocker) -> Callable:
     return _method
 
 
-def test_yfinance_security_info_provider(ticker_mocker):
-    ticker_mocker(
+def test_yfinance_security_info_provider(ticker_info, ticker_splits):
+    ticker_info(
         {"shortName": "Amazon", "currentPrice": "199.46", "currency": "USD"},
-        AMZN_PSERIES,
     )
+    ticker_splits(AMZN_PSERIES)
     provider = YahooFinanceSecurityInfoProvider()
     security_info = provider.get_info(ISIN("AMZN-ISIN"))
     assert security_info.name == "Amazon"
@@ -65,23 +70,24 @@ def test_yfinance_security_info_provider(ticker_mocker):
     assert provider.get_price(ISIN("AMZN-ISIN")) == Money("199.46", USD)
 
 
-def test_yfinance_security_info_provider_security_not_found(ticker_mocker):
-    ticker_mocker(yfinance.exceptions.YFException, [])
+def test_yfinance_security_info_provider_security_not_found(ticker_info, ticker_splits):
+    ticker_info(yfinance.exceptions.YFException)
+    ticker_splits([])
     security_info_provider = YahooFinanceSecurityInfoProvider()
     with pytest.raises(DataProviderError):
         security_info_provider.get_info(ISIN("NOT-FOUND"))
 
 
-def test_yfinance_security_info_provider_price_in_GBp(ticker_mocker):
-    ticker_mocker(
+def test_yfinance_security_info_provider_price_in_GBp(ticker_info):
+    ticker_info(
         {"currentPrice": 1550, "currency": "GBp"},
     )
     provider = YahooFinanceSecurityInfoProvider()
     assert provider.get_price(ISIN("AMZN-ISIN")) == Money("15.50", GBP)
 
 
-def test_yfinance_security_info_provider_exception_raised(ticker_mocker):
-    ticker_mocker(yfinance.exceptions.YFException)
+def test_yfinance_security_info_provider_exception_raised(ticker_info):
+    ticker_info(yfinance.exceptions.YFException)
     provider = YahooFinanceSecurityInfoProvider()
     with pytest.raises(DataProviderError):
         provider.get_info(ISIN("AMZN-ISIN"))
@@ -89,29 +95,29 @@ def test_yfinance_security_info_provider_exception_raised(ticker_mocker):
         provider.get_price(ISIN("AMZN-ISIN"))
 
 
-def test_yfinance_security_info_provider_missing_field(ticker_mocker):
-    ticker_mocker({})
+def test_yfinance_security_info_provider_missing_field(ticker_info):
+    ticker_info({})
     provider = YahooFinanceSecurityInfoProvider()
     with pytest.raises(DataProviderError):
         provider.get_info(ISIN("AMZN-ISIN"))
 
 
-def test_yfinance_live_exchange_rate_provider(ticker_mocker):
-    ticker_mocker({"bid": 0.75})
+def test_yfinance_live_exchange_rate_provider(ticker_info):
+    ticker_info({"bid": 0.75})
     provider = YahooFinanceLiveExchangeRateProvider()
     fx_rate = provider.get_rate(USD, GBP)
     assert fx_rate == Decimal("0.75")
 
 
-def test_yfinance_live_exchange_rate_provider_exception_raised(ticker_mocker):
-    ticker_mocker(yfinance.exceptions.YFException)
+def test_yfinance_live_exchange_rate_provider_exception_raised(ticker_info):
+    ticker_info(yfinance.exceptions.YFException)
     provider = YahooFinanceLiveExchangeRateProvider()
     with pytest.raises(DataProviderError):
         provider.get_rate(USD, GBP)
 
 
-def test_yfinance_live_exchange_rate_provider_missing_field(ticker_mocker):
-    ticker_mocker({})
+def test_yfinance_live_exchange_rate_provider_missing_field(ticker_info):
+    ticker_info({})
     provider = YahooFinanceLiveExchangeRateProvider()
     with pytest.raises(DataProviderError):
         provider.get_rate(USD, GBP)
